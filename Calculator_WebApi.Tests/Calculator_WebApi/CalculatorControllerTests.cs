58c18c5deeaba6902ffbdeea9d00c3d72e296c7e
Артём Кондратьев <exitway12@gmail.com>
2022-01-25 15:35:47 +0300

    CWA-AddUnitTests: Add CalculatorControllerTests class (need to realize)

---
+++ Calculator_WebApi.Tests/Calculator_WebApi/CalculatorControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_WebApi.Tests.Calculator_WebApi
+{
+    [TestFixture]
+    public class CalculatorControllerTests
+    {
+        [Test]
+        public void PostTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
