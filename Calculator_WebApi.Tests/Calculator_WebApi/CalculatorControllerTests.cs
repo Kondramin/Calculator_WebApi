0b906418d13dc8ef592fb4ed3e597dbc427d59fa
Артём Кондратьев <exitway12@gmail.com>
2022-01-25 16:33:15 +0300

    CWA-AddUnitTests: Realized CalculatorController Test

--- Calculator_WebApi.Tests/Calculator_WebApi/CalculatorControllerTests.cs
+++ Calculator_WebApi.Tests/Calculator_WebApi/CalculatorControllerTests.cs
@@ -1,8 +1,9 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Calculator_WebApi.Controllers;
+using Calculator_WebApi.Core;
+using Calculator_WebApi.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
 using System.Threading.Tasks;
 
 namespace Calculator_WebApi.Tests.Calculator_WebApi
@@ -11,14 +12,43 @@ namespace Calculator_WebApi.Tests.Calculator_WebApi
     public class CalculatorControllerTests
     {
         [Test]
-        public void PostTest()
+        public void PostIsNotNullTest()
         {
             //Arrange
+            var calculatorController = new CalculatorController(CreatMock().Object);
 
             //Act
+            var result = calculatorController.Post(new Calculator());
+
 
             //Assert
+            Assert.IsNotNull(result);
+        }
 
+
+        //[Test]
+        //public void PostReturnValuseTest()
+        //{
+        //    //Arrange
+        //    var calculatorController = new CalculatorController(CreatMock().Object);
+        //    Task<ActionResult<int>> expected = calculatorController.Post(new Calculator());
+
+        //    //Act
+        //    var result = calculatorController.Post(new Calculator());
+
+
+        //    //Assert
+        //    Assert.AreEqual(expected, result);
+        //}
+
+
+
+        private Mock<IMathOperationService> CreatMock()
+        {
+            var mock = new Mock<IMathOperationService>();
+            mock.Setup(s => s.DoMathOperation(new Calculator())).Returns(999);
+
+            return mock;
         }
     }
 }
